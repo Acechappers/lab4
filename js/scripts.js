100ebd0df7693326f56cb68472f644072f32dfb9
P17161472 <P17161472@my365.dmu.ac.uk>
2020-02-11 14:22:54 +0000

    Lab 4 work to be completed

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,26 @@
 console.log("hello");
+
+const = myButton document.querySelector("menuButton");
+console.log(myButton);
+const myNav = document.querySelector("nav");
+
+myButton.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if(myNav.classList.contains("hide")) {
+    myNav.classList.remove("hide");
+  } else {
+    myNav.classList.add("hide");
+  }
+  myNav.classList.toggle("hide");
+  myNav.style.display: none;
+  }
+
+  const = myButton2 = document.querySelector("pLfirst-of-type");
+  console.log(myButton2);
+
+  const = myButton3 = document.getElementsByClassName("my_button")
+  console.log(myButton3);
+  console.log(myButton3[0]);
+  }
+}
